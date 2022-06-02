f5dd6c67b8f73d6267c54f04d2c606922be2c983
HamdaanAliQuatil <q.hmd08@gmail.com>
2022-06-02 08:31:28 +0530

    refactor: update ui of bmi screen

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:cuppertino_bmi/util.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -20,28 +21,49 @@ class _BmiState extends State<Bmi> {
         middle: Text('BMI'),
       ),
       child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Image.network('https://bit.ly/img_bmi'),
-            Row(children: <Widget>[
-              Text('Height',
-              style: TextStyle(color: Colors.blue),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: height * 0.8,
+            ),
+            child: Column(
+              children: <Widget>[
+                Image.network('https://bit.ly/img_bmi'),
+                Util.paddingTop,
+                Row(children: <Widget>[
+                  Util.paddingLeft,
+                  Container(
+                    width: width / 3,
+                    child: Text('Height',
+                    style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                  Expanded(
+                    child: CupertinoTextField(),),
+                    Util.paddingLeft,
+                ],),
+                Util.paddingTop,
+                Row(children: <Widget>[
+                  Util.paddingLeft,
+                  Container(
+                    width: width / 3,
+                    child: Text('Weight',
+                    style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                  Expanded(
+                    child: CupertinoTextField(),),
+                    Util.paddingLeft,
+                ],),
+                Expanded(child: Util.paddingTop),
+              CupertinoButton.filled(
+                child: Text('Calculate BMI'), 
+                onPressed: (){},
               ),
-              Expanded(
-                child: CupertinoTextField(),)
-            ],),
-            Row(children: <Widget>[
-              Text('Weight',
-              style: TextStyle(color: Colors.blue),
-              ),
-              Expanded(
-                child: CupertinoTextField(),)
-            ],),
-          CupertinoButton.filled(
-            child: Text('Calculate BMI'), 
-            onPressed: (){},
+              Util.paddingTop,
+              ],
+            ),
           ),
-          ],
         )
       ),
     );
